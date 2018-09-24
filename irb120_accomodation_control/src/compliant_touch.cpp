9fa3cb63c37010971f2ed19859b423213796fbbb
matthaberbusch <mgh52@case.edu>
2018-09-24 13:05:56 -0400

    create compliant_touch.cpp

---
+++ irb120_accomodation_control/src/compliant_touch.cpp
@@ -0,0 +1,42 @@
+// Created 0/24/18
+// Matthew Haberbusch
+
+#include <ros/ros.h> //Must include this for all ROS cpp projects
+#include <sensor_msgs/LaserScan.h>
+#include <geometry_msgs/WrenchStamped.h>
+#include <std_msgs/Float64.h> 
+
+const double K_ = 0.01;
+const double DT_ = 0.001;
+const double VEL_CMD_ = 0.3; 
+
+double pos_ = 1.1;
+double vel_ = 0.0; 
+double force_ = 0.0;
+
+ros::Publisher arm_position_publisher_; //this makes it global?
+
+void forceCallback(const geometry_msgs::WrenchStamped& ft_sensor) {
+	force_ = ft_sensor.wrench.force.y; 
+
+	vel_ = VEL_CMD_ - K_ * force_; 
+	pos_ = pos_ + vel_ * DT_;
+
+	std_msgs::Float64 arm_position_msg; 
+	arm_position_msg.data = pos_;
+	arm_position_publisher_.publish(arm_position_msg);
+}
+
+int main(int argc, char **argv) {
+	ros::init(argc, argv, "compliant_touch"); //name of this node
+	ros::NodeHandle nh;
+
+	ros::Publisher pub = nh.advertise<std_msgs::Float64>("/irb120/joint2_position_controller/command", 1);
+	arm_position_publisher_ = pub; //this makes it global?
+
+	ros::Subscriber force_subscriber = nh.subscribe("ft_sensor_topic", 1, forceCallback);
+
+	ros::spin();
+	return 0;
+}
+
