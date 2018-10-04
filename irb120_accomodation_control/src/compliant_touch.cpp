59bfa877202b898af6f40c766a277ba36c26d76f
matthaberbusch <mgh52@case.edu>
2018-10-04 11:45:25 -0400

    update compliant_touch.cpp

--- irb120_accomodation_control/src/compliant_touch.cpp
+++ irb120_accomodation_control/src/compliant_touch.cpp
@@ -1,4 +1,4 @@
-// Created 0/24/18
+// Created 9/24/18
 // Matthew Haberbusch
 
 #include <ros/ros.h> //Must include this for all ROS cpp projects
@@ -7,19 +7,19 @@
 #include <std_msgs/Float64.h> 
 
 const double K_ = 0.01;
-const double DT_ = 0.001;
-const double VEL_CMD_ = 0.3; 
+const double DT_ = 0.01;
+const double VEL_CMD_ = 0.1; 
 
-double pos_ = 1.1;
+double pos_ = 1.1; //start it close to the ground
 double vel_ = 0.0; 
 double force_ = 0.0;
 
 ros::Publisher arm_position_publisher_; //this makes it global?
 
 void forceCallback(const geometry_msgs::WrenchStamped& ft_sensor) {
-	force_ = ft_sensor.wrench.force.y; 
+	force_ = ft_sensor.wrench.force.z; 
 
-	vel_ = VEL_CMD_ - K_ * force_; 
+	vel_ = VEL_CMD_ - K_ * -force_; //changed force to negative
 	pos_ = pos_ + vel_ * DT_;
 
 	std_msgs::Float64 arm_position_msg; 
@@ -35,8 +35,14 @@ int main(int argc, char **argv) {
 	arm_position_publisher_ = pub; //this makes it global?
 
 	ros::Subscriber force_subscriber = nh.subscribe("ft_sensor_topic", 1, forceCallback);
-
-	ros::spin();
-	return 0;
+    
+    ros::Rate naptime(100);// in hz
+    
+    while(ros::ok()) {
+		ros::spinOnce();
+		naptime.sleep();
+	}
+	
+	//ros::spin();
 }
-
+    
